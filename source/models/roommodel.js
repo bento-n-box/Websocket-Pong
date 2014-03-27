d508003c8bd61f787ce15ea0ba19bed9c9674778
Benton <bentonrochester@gmail.com>
2014-03-27 10:01:12 -0600

    removed collection

--- source/models/roommodel.js
+++ source/models/roommodel.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 	RoomSchema = mongoose.Schema({
 		title: String,
 		type: String
-	}),
+	}, {
+  
+    collection : 'nodePong',
+  }),
 
 RoomModel = mongoose.model('room', RoomSchema); // user is the mongo collection name
 
